995f49e1fed91af88ec645245333a7123228071d
Rohan167 <rohan167k@gmail.com>
2019-12-02 14:44:57 +0530

    Update local.py

--- trydjango_1/settings/local.py
+++ trydjango_1/settings/local.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from .base import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -25,7 +26,7 @@ SECRET_KEY = 'tq$+2u6#rnp*!1^pi+b!p7nx-@8fo%%ry%g+1_^jr3_5d93q18'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
